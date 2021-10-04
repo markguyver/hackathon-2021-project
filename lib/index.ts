e3e45df5edee51256e2a9e11ed063ef8be2dab6a
Mark Chavez <mark.chavez@pager.com>
2021-10-04 11:58:41 -0600

    Fixing whoopsie

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,12 @@
 import fastify, { FastifyInstance } from 'fastify';
 import { ApplicationConfig } from './config';
 import registerGraphql from './graphql';
-import registerRestfulRouts from './routes';
+import registerRestfulRoutes from './routes';
 
 const buildApplication = async (config: ApplicationConfig): Promise<FastifyInstance> => {
     const application = fastify(config.fastify.options);
     await registerGraphql(application);
-    registerGraphql(application);
+    registerRestfulRoutes(application);
     return application;
 };
 
