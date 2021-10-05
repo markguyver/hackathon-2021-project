0aa9435259a55ddf1f2974c19af67d9e7d060efe
Mark Chavez <mark.chavez@pager.com>
2021-10-05 13:57:49 -0600

    Filtering sensitive config items in logging

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,17 @@ import { FastifyInstance } from 'fastify';
 import buildApplication from "../lib";
 import defaultApplicationConfig, { ApplicationConfig } from '../lib/config';
 
+const filterSensitiveConfigFields = (config: ApplicationConfig): ApplicationConfig => {
+    const filteredConfig = { ...config };
+    filteredConfig.database.password = '[redacted]';
+    return filteredConfig;
+};
+
 const createServer = async (start: boolean = false, config: ApplicationConfig = defaultApplicationConfig): Promise<FastifyInstance> => {
     const application = await buildApplication(config);
     if (start) {
         await application.listen(config.fastify.port, config.fastify.host);
-        application.log.debug({ applicationConfig: config }, 'Application Configuration');
+        application.log.debug({ applicationConfig: filterSensitiveConfigFields(config) }, 'Application Configuration');
     }
     return application;
 };
